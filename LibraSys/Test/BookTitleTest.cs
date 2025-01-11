bd81226625053f932fe8a4a87a268a3a96da834d
EhsanMrf <developermrf@gmail.com>
2025-01-11 11:52:19 +0330

    add test for domain

---
+++ LibraSys/Test/BookTitleTest.cs
@@ -0,0 +1,52 @@
+using Domian.Model.Book;
+using FluentAssertions;
+
+namespace Test
+{
+    public class BookTitleTest
+    {
+        [Fact]
+        public void should_throw_exception_if_title_null()
+        {
+            var courser = () =>
+            {
+                BookTitle.CreateInstance(string.Empty);
+            };
+
+            courser.Should().Throw<BookException.BookTitleNullException>();
+        }
+        
+        [Fact]
+        public void should_throw_exception_if_title_less_two()
+        {
+            var courser = () =>
+            {
+                BookTitle.CreateInstance("A");
+            };
+
+            courser.Should().Throw<BookException.BookTitleLengthException>();
+        }
+
+        [Fact]
+        public void should_throw_exception_if_title_equalto_two()
+        {
+            var courser = () =>
+            {
+                BookTitle.CreateInstance("Ab");
+            };
+
+            courser.Should().Throw<BookException.BookTitleLengthException>();
+        }
+
+        [Fact]
+        public void should_instance_with_out_exception()
+        {
+            var courser = () =>
+            {
+                BookTitle.CreateInstance("Book Title");
+            };
+
+            courser.Should().NotThrow();
+        }
+    }
+}
